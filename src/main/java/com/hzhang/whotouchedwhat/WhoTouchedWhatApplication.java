afb4982e57b75aff13c8e6b47bf168c9422157d2
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-12 12:58:17 -0400

    first commit

---
+++ src/main/java/com/hzhang/whotouchedwhat/WhoTouchedWhatApplication.java
@@ -0,0 +1,13 @@
+package com.hzhang.whotouchedwhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoTouchedWhatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoTouchedWhatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzhang/whotouchedwhat/WhoTouchedWhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzhang.whotouchedwhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoTouchedWhatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
